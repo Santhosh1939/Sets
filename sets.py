f6315c44bbad82f0a3ace18cc60bf7b25647d280
SANTHOSH KUMAR GOWDA <gowda.santhu1939@gmail.com>
2025-04-01 19:35:59 -0400

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,61 @@
+# # Write Python code to find and print the intersection of the following two sets:
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {4, 5, 6, 7, 8}
+# set3=set1.intersection(set2)
+# print(set3)
+# # Output : {4, 5}
+
+# # Task 2: Set UnionWrite Python code to find and print the union of the following two sets:
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {4, 5, 6, 7, 8}
+# set3=set1.union(set2)
+# print(set3)
+# # Output : {1, 2, 3, 4, 5, 6, 7, 8}
+
+# # Task 3: Set Difference Write Python code to find and print the elements present in set1 but not in set2 :
+# # set1 = {1, 2, 3, 4, 5}
+# # set2 = {4, 5, 6, 7, 8}
+# set3=set1.difference(set2)
+# print(set3)
+# # Output : {1, 2, 3}
+
+# Task 4: Set Symmetric Difference
+# Write Python code to find and print the symmetric difference of the following two sets:
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {4, 5, 6, 7, 8}
+# set3=set1.symmetric_difference(set2)
+# print(set3)
+# # Output: {1, 2, 3, 6, 7, 8}
+
+# # Task 5: Set Membership Test
+# # Write Python code to check if the element 3 is present in the set my_set :
+# my_set = {1, 2, 3, 4, 5}
+# print((3)in my_set)
+# # Output: True
+
+
+# Exercise 1: Set Intersection
+# Write a Python script that finds and prints the intersection of two sets.
+set_1={"KS","GS","19","39"}
+set_2={"ball","bat","19","39"}
+# set_3=set_1.intersection(set_2)
+# print(set_3)
+# output:{'19', '39'}
+
+# Exercise 2: Set Union
+# Write a Python script that finds and prints the union of two sets.
+# set_3=set_1.union(set_2)
+# print(set_3)
+# output: {'KS', '39', 'bat', 'ball', 'GS', '19'}
+
+# Exercise 3: Set Difference
+# Write a Python script that finds and prints the difference between two sets.
+# set_3=set_1.difference(set_2)
+# print(set_3)
+# output:{'KS', 'GS'}
+
+# Exercise 4: Set Symmetric Difference
+# Write a Python script that finds and prints the symmetric difference between two sets.
+# set_3=set_1.symmetric_difference(set_2)
+# print(set_3)
+# output:{'bat', 'ball', 'GS', 'KS'}
